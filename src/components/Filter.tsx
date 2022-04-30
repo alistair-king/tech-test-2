5efa218cf9352b22b8ee712db8a6844d709346eb
Alistair King <mr.alistairk.king@gmail.com>
2022-04-30 17:20:39 +1000

    Obsessive reduction of code duplication

--- src/components/Filter.tsx
+++ src/components/Filter.tsx
@@ -1,3 +1,4 @@
+import { ReactNode } from 'react'
 import cls from 'classnames'
 
 import { Status } from '../types'
@@ -11,45 +12,61 @@ const Filter = ({
 }) => (
   <div className="flex justify-center mb-4">
     <div className="inline-flex rounded-md shadow-sm" role="group">
-      <button
-        type="button"
-        onClick={() => setFilterStatus('all')}
-        className={cls({
-          'py-2 px-4 text-sm font-medium rounded-l-lg border border-gray-200': true,
-          'text-gray-900 bg-white': (filterStatus !== 'all'),
-          'text-white bg-gray-900': (filterStatus === 'all')
-        })}
+      <Button
+        buttonStatus="all"
+        className="py-2 px-4 text-sm font-medium rounded-l-lg border border-gray-200"
+        filterStatus={filterStatus}
+        setFilterStatus={setFilterStatus}
       >
         All
-      </button>
-      <button
-        type="button"
-        onClick={() => setFilterStatus('online')}
-        className={cls({
-          'py-2 px-4 text-sm font-medium border-t border-b border-gray-200': true,
-          'text-gray-900 bg-white': (filterStatus !== 'online'),
-          'text-white bg-gray-900': (filterStatus === 'online')
-        })}
+      </Button>
+      <Button
+        buttonStatus="online"
+        className="py-2 px-4 text-sm font-medium border-t border-b border-gray-200"
+        filterStatus={filterStatus}
+        setFilterStatus={setFilterStatus}
       >
         Online Only
-      </button>
-      <button
-        type="button"
-        onClick={() => setFilterStatus('offline')}
-        className={
-          cls({
-            'py-2 px-4 text-sm font-medium rounded-r-md border border-gray-200': true,
-            'text-gray-900 bg-white': (filterStatus !== 'offline'),
-            'text-white bg-gray-900': (filterStatus === 'offline')
-          })          
-        }
+      </Button>
+      <Button
+        buttonStatus="offline"
+        className="py-2 px-4 text-sm font-medium rounded-r-md border border-gray-200"
+        filterStatus={filterStatus}
+        setFilterStatus={setFilterStatus}
       >
         Offline Only
-      </button>
+      </Button>
     </div>
   </div>
 )
 
+const Button = ({
+  children,
+  className,
+  filterStatus,
+  buttonStatus,
+  setFilterStatus
+}: {
+  children: ReactNode
+  className: string
+  filterStatus: Status
+  buttonStatus: Status
+  setFilterStatus: (status: Status) => void
+}) => (
+      <button
+        type="button"
+        onClick={() => setFilterStatus(buttonStatus)}
+        className={
+          cls({
+            [className]: true,
+            'text-gray-900 bg-white': (filterStatus !== buttonStatus),
+            'text-white bg-gray-900': (filterStatus === buttonStatus)
+          })          
+        }
+      >
+        {children}
+      </button>
 
+)
 
 export default Filter
